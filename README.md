909826db38373b5df7a5f36e7bd7f678da04aa46
anas2233 <aljawabrah2022@gmail.com>
2023-08-04 02:12:10 +0300

    new changes

--- README.md
+++ README.md
@@ -1,3 +1,33 @@
-# IDAssignmenTwo
-# IDAssignmenTwo
-# IDAssignmenTwo
+## IDAssignmenTwo
+
+#### Download SortExample.java code from E-learning Moodle. Then perform the following:
+- Create a folder on your machine (D:\), name it with your IDAssignmenTwo, and Initiate a Git repository in the folder. 
+
+<br>
+
+- Re-format the code such that it conforms with layout and styling guidelines presented in 
+the lectures.
+
+<br>
+
+-  Use Java standard annotation presented in the lectures to document SortExample.java and 
+its methods. Then use Javadoc utility to generate standard documentation for the 
+algorithms. From command line cmd or powershell type:
+    ```
+     javacode -d  /path/to/output /path/to/*.java
+    ```
+
+<br>
+
+- Finally, stage and store the file in the Git repository. 
+
+<br>
+
+- Compress the folder as zip file and submit it using E-learning Moodl
+
+<br>
+
+---
+<br>
+
+**Anas Jawabreh**
--- SortExample.java
+++ SortExample.java
@@ -1,108 +1,155 @@
-import java.util.*;
-public class SortExample 
-{
- public static void main(String[] args) 
- {
- // This is unsorted array
- Integer[] array1 = new Integer[] { 12, 13, 24, 10, 3, 6, 90, 70 };
- int array2[] = { 2, 6, 3, 5, 1 };
- // Let's sort using quick sort
- quickSort( array1, 0, array1.length - 1 );
- // Verify sorted array
- System.out.println(Arrays.toString(array1));
- mergeSort(array2, array2.length - 1 );
- // Verify sorted array
- System.out.println(Arrays.toString(array2));
- }
- public static void quickSort(Integer[] arr, int low, int high) 
- {
- //check for empty or null array
- if (arr == null || arr.length == 0){
- return;
- }
- if (low >= high){
- return;
- }
- //Get the pivot element from the middle of the list
- int middle = low + (high - low) / 2;
- int pivot = arr[middle];
- // make left < pivot and right > pivot
- int i = low, j = high;
- while (i <= j) 
- {
- //Check until all values on left side array are lower than pivot
- while (arr[i] < pivot) 
- {
- i++;
- }
- //Check until all values on left side array are greater than pivot
- while (arr[j] > pivot) 
- {
- j--;
- }
- //Now compare values from both side of lists to see if they need swapping 
- //After swapping move the iterator on both lists
- if (i <= j) 
- {
- swap (arr, i, j);
- i++;
- j--;
- }
- }
- //Do same operation as above recursively to sort two sub arrays
- if (low < j){
- quickSort(arr, low, j);
- }
- if (high > i){
- quickSort(arr, i, high);
- }
- }
- public static void swap (Integer array[], int x, int y)
- {
- int temp = array[x];
- array[x] = array[y];
- array[y] = temp;
- }
-public static void mergeSort(int[] a, int n) {
- if (n < 2) {
- return;
- }
- int mid = n / 2;
- int[] l = new int[mid];
- int[] r = new int[n - mid];
- for (int i = 0; i < mid; i++) {
- l[i] = a[i];
- }
- for (int i = mid; i < n; i++) {
- r[i - mid] = a[i];
- }
- mergeSort(l, mid);
- mergeSort(r, n - mid);
- merge(a, l, r, mid, n - mid);
-}
-public static void merge(
- int[] a, int[] l, int[] r, int left, int right) {
- int i = 0, j = 0, k = 0;
- while (i < left && j < right) {
- if (l[i] <= r[j]) {
- a[k++] = l[i++];
- }
- else {
- a[k++] = r[j++];
- }
- }
- while (i < left) {
- a[k++] = l[i++];
- }
- while (j < right) {
- a[k++] = r[j++];
- }
-}
-private static boolean isSorted(int[] x)
- {
- for (int i = 0; i < x.length - 1; i++)
- if (x[i] > x[i + 1])
- return false;
- return true;
- }
+import java.util.Arrays;
+
+/**
+ * This class demonstrates various sorting algorithms in Java.
+ */
+public class SortExample {
+
+    public static void main(String[] args) {
+        // This is an unsorted array
+        Integer[] array1 = new Integer[]{12, 13, 24, 10, 3, 6, 90, 70};
+        int array2[] = {2, 6, 3, 5, 1};
+
+        // Let's sort using quick sort
+        quickSort(array1, 0, array1.length - 1);
+        // Verify sorted array
+        System.out.println(Arrays.toString(array1));
+
+        mergeSort(array2, array2.length - 1);
+        // Verify sorted array
+        System.out.println(Arrays.toString(array2));
+    }
+
+    /**
+     * Sorts an array using the quick sort algorithm.
+     *
+     * @param arr  the array to be sorted.
+     * @param low  the starting index of the array.
+     * @param high the ending index of the array.
+     */
+    public static void quickSort(Integer[] arr, int low, int high) {
+        // Check for an empty or null array
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+
+        if (low >= high) {
+            return;
+        }
+
+        // Get the pivot element from the middle of the list
+        int middle = low + (high - low) / 2;
+        int pivot = arr[middle];
+
+        // Make left < pivot and right > pivot
+        int i = low, j = high;
+        while (i <= j) {
+            // Check until all values on the left side of the array are lower than the pivot
+            while (arr[i] < pivot) {
+                i++;
+            }
+            // Check until all values on the right side of the array are greater than the pivot
+            while (arr[j] > pivot) {
+                j--;
+            }
+            // Now compare values from both sides of the lists to see if they need swapping
+            // After swapping, move the iterator on both lists
+            if (i <= j) {
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+        }
+        // Do the same operation as above recursively to sort two sub-arrays
+        if (low < j) {
+            quickSort(arr, low, j);
+        }
+        if (high > i) {
+            quickSort(arr, i, high);
+        }
+    }
+
+    /**
+     * Swaps two elements in an array.
+     *
+     * @param array the array where the elements need to be swapped.
+     * @param x     the index of the first element to swap.
+     * @param y     the index of the second element to swap.
+     */
+    public static void swap(Integer array[], int x, int y) {
+        int temp = array[x];
+        array[x] = array[y];
+        array[y] = temp;
+    }
+
+    /**
+     * Sorts an array using the merge sort algorithm.
+     *
+     * @param a the array to be sorted.
+     * @param n the number of elements in the array.
+     */
+    public static void mergeSort(int[] a, int n) {
+        if (n < 2) {
+            return;
+        }
+
+        int mid = n / 2;
+        int[] l = new int[mid];
+        int[] r = new int[n - mid];
+
+        for (int i = 0; i < mid; i++) {
+            l[i] = a[i];
+        }
+        for (int i = mid; i < n; i++) {
+            r[i - mid] = a[i];
+        }
+
+        mergeSort(l, mid);
+        mergeSort(r, n - mid);
+        merge(a, l, r, mid, n - mid);
+    }
+
+    /**
+     * Merges two sub-arrays into a single array.
+     *
+     * @param a     the array to store the merged elements.
+     * @param l     the left sub-array.
+     * @param r     the right sub-array.
+     * @param left  the number of elements in the left sub-array.
+     * @param right the number of elements in the right sub-array.
+     */
+    public static void merge(int[] a, int[] l, int[] r, int left, int right) {
+        int i = 0, j = 0, k = 0;
+
+        while (i < left && j < right) {
+            if (l[i] <= r[j]) {
+                a[k++] = l[i++];
+            } else {
+                a[k++] = r[j++];
+            }
+        }
+
+        while (i < left) {
+            a[k++] = l[i++];
+        }
+
+        while (j < right) {
+            a[k++] = r[j++];
+        }
+    }
+
+    /**
+     * Checks if an array is sorted in ascending order.
+     *
+     * @param x the array to be checked.
+     * @return true if the array is sorted, false otherwise.
+     */
+    private static boolean isSorted(int[] x) {
+        for (int i = 0; i < x.length - 1; i++) {
+            if (x[i] > x[i + 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
